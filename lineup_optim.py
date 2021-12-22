4036b10a47655fb68f26e0b30d5db3be1f99d0af
spdtek <96366558+spdtek@users.noreply.github.com>
2021-12-22 09:24:02 +0100

    Update lineup_optim.py

--- lineup_optim.py
+++ lineup_optim.py
@@ -1,4 +1,4 @@
-# Copyright [2021] [Promoción y Desarrollo de Sistemas Automáticos SLU]
+# Copyright 2021 Promoción y Desarrollo de Sistemas Automáticos SLU
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
